aebc55361b392accaa37896b7ecb9f599f888a7d
Leonard Souza <leonard.souza@spatialkey.com>
2015-07-06 07:17:29 -0700

    removed semi-colons

--- javascript/config/Bootstrap.js
+++ javascript/config/Bootstrap.js
@@ -5,31 +5,31 @@ import Main from '../views/Main.jsx'
 class Bootstrap {
 
     constructor() {
-        this.win = gui.Window.get();
+        this.win = gui.Window.get()
     }
 
     initialize() {
 
-        let menuBar = new gui.Menu({type:"menubar"});
-        menuBar.createMacBuiltin("NW.js Demo");
-        this.win.menu = menuBar;
+        let menuBar = new gui.Menu({type:"menubar"})
+        menuBar.createMacBuiltin("NW.js Demo")
+        this.win.menu = menuBar
 
-        let developMenu = new gui.Menu();
+        let developMenu = new gui.Menu()
         let developToggleitem = new gui.MenuItem({
             label: 'Toggle Dev Tools',
             click: function() {
                 if (this.win.isDevToolsOpen())
-                    this.win.closeDevTools();
+                    this.win.closeDevTools()
                 else
-                    this.win.showDevTools();
+                    this.win.showDevTools()
             }
-        });
+        })
 
-        developMenu.append(developToggleitem);
+        developMenu.append(developToggleitem)
         this.win.menu.append(new gui.MenuItem({
             label: 'Develop',
             submenu: developMenu
-        }));
+        }))
 
         this.win.on("loaded", () => {
 
